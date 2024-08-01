8ecb48f744d18a216b576b2bd293c86bc9833d94
Alex Ledesma <alex.ledesma7@gmail.com>
2024-07-31 23:53:22 -0400

    adding getters and setters as well as function to pull the instance name from the tags provided

--- instance.py
+++ instance.py
@@ -13,3 +13,38 @@ class Instance():
         self.ami_id         = ami_id
         self.ami_name       = ami_name
         self.age            = age
+
+    def get_instance_name(self):
+        return self.instance_name
+    
+    def get_instance_id(self):
+        return self.instance_id
+    
+    def get_ami_id(self):
+        return self.ami_id
+    
+    def get_ami_name(self):
+        return self.ami_name
+    
+    def get_age(self):
+        return self.age
+    
+    def set_instance_name(self, instance_name):
+        self.instance_name = instance_name
+
+    def set_instance_id(self, instance_id):
+        self.instance_id = instance_id
+
+    def set_ami_id(self, ami_id):
+        self.ami_id = ami_id
+
+    def set_ami_name(self, ami_name):
+        self.ami_name = ami_name
+
+    def set_age(self, age):
+        self.age = age
+
+def get_isntance_name(tags):
+    for tag in tags:
+        if tag["Key"] == "Name":
+            return tag["Value"]
